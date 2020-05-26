1b95acefedf1571d35a3ac5016bef34afb0561b0
Emily <emilymnicholas@gmail.com>
2020-05-26 03:49:12 +0000

    completed 3_queries

---
+++ 3_queries_group-by/cohort_count.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort_name, count(*) as student_count
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+HAVING count(*) >= 18
+ORDER BY student_count
+;
---
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name as cohort_name, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
+;
+
