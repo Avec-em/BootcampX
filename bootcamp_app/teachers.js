fc9f296d60a19b1441d6b4cd5f043f09b16befed
Emily <emilymnicholas@gmail.com>
2020-05-28 04:10:55 +0000

    protected against sql Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,3 @@
-
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -10,21 +8,23 @@ const pool = new Pool({
 },
 console.log('connected'));
 
-
-pool.query(`
+const queryString = `
 SELECT teachers.name AS name, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name
 ;
-`)
+`
+const teacherName = [`${process.argv[2]}`];
+
+
+pool.query(queryString, teacherName)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.name}`);
   })
 }).catch(err => console.error('query error', err.stack));
-
